768b27c4d442dfa0c2b276677036f1a6cc31f236
VehWare-PACE-DDP <44152115+VehWare-PACE-DDP@users.noreply.github.com>
2018-10-18 23:36:08 -0400

    Update cursorTracker.py
    Make things explicit.

--- cursorTracker.py
+++ cursorTracker.py
@@ -1,2 +1,11 @@
-import pyautogui
-pyautogui.displayMousePosition()
+import pyautogui, sys
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x) + ' Y: ' + str(y)
+        print positionStr,
+        print '\b' * (len(positionStr) + 2),
+        sys.stdout.flush()
+except KeyboardInterrupt:
+    print '\n'
